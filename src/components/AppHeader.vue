12b4596db09805fd449edefbb08f173d29badffd
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-06-05 16:38:18 +0200

    first structure header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name : "AppHeader",
+    props: ["msg"]
+}
+</script>
+
+<template>
+
+<header>
+    <div class="img-container">
+      <img src="../../public/yugioh_logo.png" alt="yugioh logo">
+    </div>
+    <h1>{{ msg }}</h1>
+</header>
+
+</template>
+
+<style scoped lang="scss">
+    header{
+      height: 100px;
+      display: flex;
+      align-items: center;
+
+      .img-container{
+        width: 80px;
+        margin: 0 25px;
+      }
+    }
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    name : "AppMain"
+
+}
+</script>
+
+<template>
+
+    <main>
+
+        <div class="cover">
+
+            <select class="dropdown">
+            <option value="Alien" selected>Alien</option>
+            </select>
+
+        </div>
+
+        <div class="cards-container">
+
+        </div>
+
+    </main>
+
+</template>
+
+<style  lang="scss">
+    .main{
+        background-color: #d48f38;
+        height: calc(100vh - 100px);
+
+        .dropdown{
+            padding: 10px;
+            background-color: white;
+            border: none;
+            border-radius: 10px;
+            width: 200px;
+            margin: 25px;
+        }
+    }
+
+    .cards-container{
+        width: 80%;
+        height: 100%;
+        margin: auto;
+        background-color: white;
+    }
+
+</style>
